9be3d872be0417d3c7bb35430e1c8a5e8b03c11f
reefuu <rifqiet@gmail.com>
2023-04-11 08:16:25 +0700

    Initial Commit

---
+++ 0706012110025-Rifqie-AFL3/_706012110025_Rifqie_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110025_Rifqie_AFL3App.swift
+//  0706012110025-Rifqie-AFL3
+//
+//  Created by Rifqie Tilqa Reamizard on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110025_Rifqie_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
