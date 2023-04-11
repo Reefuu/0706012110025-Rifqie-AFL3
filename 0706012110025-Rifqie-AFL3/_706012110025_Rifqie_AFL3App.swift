cf9ea66cde56faf9e57c4eb9cea3d213c56b3031
reefuu <rifqiet@gmail.com>
2023-04-11 10:06:29 +0700

    CHAPTER 1 DONE

--- 0706012110025-Rifqie-AFL3/_706012110025_Rifqie_AFL3App.swift
+++ 0706012110025-Rifqie-AFL3/_706012110025_Rifqie_AFL3App.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
+
 @main
 struct _706012110025_Rifqie_AFL3App: App {
+    
+    @StateObject private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
+
         }
     }
 }
