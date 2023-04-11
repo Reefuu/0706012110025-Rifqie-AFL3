beca6f941e7532d0f7a51a3cc50217b9b0546e5c
reefuu <rifqiet@gmail.com>
2023-04-11 08:28:51 +0700

    Section3

--- 0706012110025-Rifqie-AFL3/ContentView.swift
+++ 0706012110025-Rifqie-AFL3/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack {
+        VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
             }
                 
         }
