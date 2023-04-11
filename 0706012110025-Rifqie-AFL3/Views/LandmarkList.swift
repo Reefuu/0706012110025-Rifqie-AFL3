ae7a694a50b95f2e079194fc56f8c5298051f3bc
reefuu <rifqiet@gmail.com>
2023-04-11 09:40:24 +0700

    Section3 Part3 Chapter1

--- 0706012110025-Rifqie-AFL3/Views/LandmarkList.swift
+++ 0706012110025-Rifqie-AFL3/Views/LandmarkList.swift
@@ -8,24 +8,29 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
-
+    @State private var showFavoritesOnly = false
+    
     var filteredLandmarks: [Landmark] {
-            landmarks.filter { landmark in
-                (!showFavoritesOnly || landmark.isFavorite)
-            }
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
         }
+    }
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List() {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         }
     }
 }
@@ -33,6 +38,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-                    LandmarkList()                }
+            LandmarkList()                }
     }
 }
