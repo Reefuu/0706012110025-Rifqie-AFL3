2cc40e316142d44f044fc50c09391a24f73243c1
reefuu <rifqiet@gmail.com>
2023-04-18 09:32:17 +0700

    Chapter3 Part2 Section1

--- 0706012110025-Rifqie-AFL3/Views/ContentView.swift
+++ 0706012110025-Rifqie-AFL3/Views/ContentView.swift
@@ -7,38 +7,28 @@
 
 import SwiftUI
 
+//shows the lists of landmark
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                
-            }
-            .padding()
+                .tag(Tab.featured)
             
-            Spacer()
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
         }
     }
 }
@@ -46,5 +36,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
+        
     }
 }
---
+++ 0706012110025-Rifqie-AFL3/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHost.swift
+//  0706012110025-Rifqie-AFL3
+//
+//  Created by Rifqie Tilqa Reamizard on 18/04/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+                    ProfileSummary(profile: draftProfile)
+                }
+                .padding()    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
