4e2ca03e2d422c0b4f27768e871c645cf3fa23e0
reefuu <rifqiet@gmail.com>
2023-04-28 08:37:56 +0700

    Chapter4 Part3 Section6

--- 0706012110025-Rifqie-AFL3/Views/Landmarks/LandmarkList.swift
+++ 0706012110025-Rifqie-AFL3/Views/Landmarks/LandmarkList.swift
@@ -12,6 +12,7 @@ struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
     @State private var filter = FilterCategory.all
+    @State private var selectedLandmark: Landmark?
     
     enum FilterCategory: String, CaseIterable, Identifiable {
         case all = "All"
@@ -31,42 +32,48 @@ struct LandmarkList: View {
     }
     
     var title: String {
-            let title = filter == .all ? "Landmarks" : filter.rawValue
-            return showFavoritesOnly ? "Favorite \(title)" : title
-        }
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
+    }
+    
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
+    }
     
     var body: some View {
         NavigationView {
-            List() {
+            List(selection: $selectedLandmark) {
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
+                    .tag(landmark)
                 }
-                .navigationTitle(title)
-                .frame(minWidth: 300)
-                .toolbar {
-                    ToolbarItem {
-                        Menu {
-                            Picker("Category", selection: $filter) {
-                                ForEach(FilterCategory.allCases) { category in
-                                    Text(category.rawValue).tag(category)
-                                }
+            }
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
                             }
-                            .pickerStyle(.inline)
-                            Toggle(isOn: $showFavoritesOnly) {
-                                Label("Favorites only", systemImage: "star.fill")
-                            }
-                        } label: {
-                            Label("Filter", systemImage: "slider.horizontal.3")
                         }
+                        .pickerStyle(.inline)
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorites only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
                     }
                 }
-                Text("Select a Landmark")
             }
+            Text("Select a Landmark")
         }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
     }
 }
 
